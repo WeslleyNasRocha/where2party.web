23ca553d371dc0decdac403d9cbcc896b4f14dfb
Weslley Rocha <weslleytato.12@gmail.com>
2017-12-12 04:24:43 +0000

    firebase.js edited online with Bitbucket

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyDhAzjuSCIob9HIXUefkOiK02GBFT0LL98',
-  authDomain: 'where2party-test.firebaseapp.com',
-  databaseURL: 'https://where2party-test.firebaseio.com',
-  projectId: 'where2party-test',
-  storageBucket: '',
-  messagingSenderId: '710263664528',
+  apiKey: "AIzaSyAiceitFuCvdfw0xATK2FYRPgpVBkp9q4E",
+  authDomain: "where2party-51f6a.firebaseapp.com",
+  databaseURL: "https://where2party-51f6a.firebaseio.com",
+  projectId: "where2party-51f6a",
+  storageBucket: "where2party-51f6a.appspot.com",
+  messagingSenderId: "519155446978"
 };
 
 firebase.initializeApp(config);
